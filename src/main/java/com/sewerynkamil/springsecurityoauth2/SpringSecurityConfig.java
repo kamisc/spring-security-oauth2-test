69d5264f4ecd97189d93203a379261ecc41c697f
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-02-26 18:41:14 +0100

    add other spring security config

--- src/main/java/com/sewerynkamil/springsecurityoauth2/SpringSecurityConfig.java
+++ src/main/java/com/sewerynkamil/springsecurityoauth2/SpringSecurityConfig.java
@@ -12,37 +12,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
-@Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+public class SpringSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder getBCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User user = new User(
-                "user",
-                getBCryptPasswordEncoder().encode("user1234"),
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
-
-        User admin = new User(
-                "admin",
-                getBCryptPasswordEncoder().encode("admin1234"),
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
-
-        auth.inMemoryAuthentication().withUser(user).withUser(admin);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/for-user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/for-admin").hasAnyRole("ADMIN")
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().logoutUrl("/bye");
-    }
+//    @Bean
+//    public PasswordEncoder getBCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        User user = new User(
+//                "user",
+//                getBCryptPasswordEncoder().encode("user1234"),
+//                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+//
+//        User admin = new User(
+//                "admin",
+//                getBCryptPasswordEncoder().encode("admin1234"),
+//                Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
+//
+//        auth.inMemoryAuthentication().withUser(user).withUser(admin);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/for-user").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/for-admin").hasAnyRole("ADMIN")
+//                .and()
+//                .formLogin().permitAll()
+//                .and()
+//                .logout().logoutUrl("/bye").permitAll();
+//    }
 }
--- src/main/java/com/sewerynkamil/springsecurityoauth2/SpringSecurityConfigNew.java
+++ src/main/java/com/sewerynkamil/springsecurityoauth2/SpringSecurityConfigNew.java
@@ -1,4 +1,51 @@
 package com.sewerynkamil.springsecurityoauth2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@Configuration
 public class SpringSecurityConfigNew {
+    @Bean
+    public PasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails user = User.withUsername("user")
+                .password(getBCryptPasswordEncoder().encode("user1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(getBCryptPasswordEncoder().encode("admin1234"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(Arrays.asList(user, admin));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((auth) ->
+                auth
+                .antMatchers("/for-user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/for-admin").hasAnyRole("ADMIN"))
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout()
+//                .deleteCookies("Cookies")
+                .logoutSuccessUrl("/bye").permitAll();
+
+        return http.build();
+    }
 }
