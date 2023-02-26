91637adb7c751e219a0e6ce973ba9db05e353ff4
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-02-26 13:32:15 +0100

    Add simple testing controller methods

--- src/main/java/com/sewerynkamil/springsecurityoauth2/TestController.java
+++ src/main/java/com/sewerynkamil/springsecurityoauth2/TestController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-//    @GetMapping
-//    public String get() {
-//
-//    }
-//
-//    @GetMapping
-//    public String get() {
-//
-//    }
-//
-//    @GetMapping
-//    public String get() {
-//
-//    }
+    @GetMapping("/for-all")
+    public String getForAll() {
+        return "Hello you!";
+    }
+
+    @GetMapping("/for-user")
+    public String getForUser() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/for-admin")
+    public String getForAdmin() {
+        return "Hello Admin!";
+    }
 }
